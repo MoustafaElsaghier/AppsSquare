48a1f843861cd81e523f820a2548b87a51e19283
M_ELSaghier <thedreamereng@gmail.com>
2018-01-05 23:03:17 +0200

    Initial commit

---
+++ app/src/main/java/elsaghier/example/com/appssquare/FirstTask.java
@@ -0,0 +1,13 @@
+package elsaghier.example.com.appssquare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_task);
+    }
+}
