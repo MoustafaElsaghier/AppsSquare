c6b2dd78099d11a3f57ad3076b1cd78cd70d6e0d
M_ELSaghier <thedreamereng@gmail.com>
2018-01-12 04:08:13 +0200

    Create layouts for Fragments

--- app/src/main/java/elsaghier/example/com/appssquare/Second_Task/Fragments/ContactUsFragment.java
+++ app/src/main/java/elsaghier/example/com/appssquare/Second_Task/Fragments/ContactUsFragment.java
@@ -1,10 +1,22 @@
 package elsaghier.example.com.appssquare.Second_Task.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import elsaghier.example.com.appssquare.R;
 
 /**
  * Created by ELSaghier on 1/12/2018.
  */
 
 public class ContactUsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.contact_us_layout,container,false);
+    }
 }
