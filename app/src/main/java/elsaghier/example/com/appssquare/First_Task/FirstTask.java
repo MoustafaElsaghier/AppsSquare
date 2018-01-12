3f0f855342c16f29da0dbd03baa64aa209aab10c
M_ELSaghier <thedreamereng@gmail.com>
2018-01-12 03:26:45 +0200

    Finish all task one Requirements ^^

--- app/src/main/java/elsaghier/example/com/appssquare/First_Task/FirstTask.java
+++ app/src/main/java/elsaghier/example/com/appssquare/First_Task/FirstTask.java
@@ -1,12 +1,11 @@
 package elsaghier.example.com.appssquare.First_Task;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import elsaghier.example.com.appssquare.R;
 
@@ -31,13 +30,23 @@ public class FirstTask extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = userName.getEditText().getText().toString();
-                String pass = userName.getEditText().getText().toString();
-                if (TextUtils.isEmpty(name))
-                    userName.setError("Please Enter Name");
-                if (TextUtils.isEmpty(pass))
-                    password.setError("Please Enter Password");
-                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(pass))
-                    Snackbar.make(view, R.string.validUserAndPass, Snackbar.LENGTH_LONG);
+                String pass = password.getEditText().getText().toString();
+                if (name.matches("")) {
+                    userName.setErrorEnabled(true);
+                    userName.setError("Enter Name");
+                } else {
+                    userName.setError(null);
+                    userName.setErrorEnabled(false);
+                }
+                if (pass.matches("")) {
+                    password.setErrorEnabled(true);
+                    password.setError("Enter Password");
+                } else {
+                    password.setError(null);
+                    password.setErrorEnabled(false);
+                }
+                if (!name.matches("") && !pass.matches(""))
+                    Toast.makeText(FirstTask.this, R.string.validUserAndPass, Toast.LENGTH_SHORT).show();
             }
         });
     }
