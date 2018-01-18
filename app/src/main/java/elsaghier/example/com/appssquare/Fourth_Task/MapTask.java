18265e754e12a7654a2cf81385fd291843c298d5
M_ELSaghier <thedreamereng@gmail.com>
2018-01-18 13:11:15 +0200

    add two markers on map

--- app/src/main/java/elsaghier/example/com/appssquare/Fourth_Task/MapTask.java
+++ app/src/main/java/elsaghier/example/com/appssquare/Fourth_Task/MapTask.java
@@ -3,7 +3,6 @@ package elsaghier.example.com.appssquare.Fourth_Task;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -24,8 +23,21 @@ public class MapTask extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                if (firstMarker == null) {
+                    firstMarker = new MarkerOptions().position(latLng).title("First Marker");
+                    mMap.addMarker(firstMarker);
+                } else {
+                    mMap.clear();
+                    mMap.addMarker(firstMarker);
+                    secondMarker = new MarkerOptions().position(latLng).title("Second Marker");
+                    mMap.addMarker(secondMarker);
+                }
+            }
+        });
     }
-
 
     /**
      * Manipulates the map once available.
@@ -36,13 +48,13 @@ public class MapTask extends FragmentActivity implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    MarkerOptions firstMarker, secondMarker;
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
