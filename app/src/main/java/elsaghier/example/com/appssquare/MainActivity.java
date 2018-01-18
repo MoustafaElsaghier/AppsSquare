bf60729dbc5cec4648190d89232e80eaff1d4e85
M_ELSaghier <thedreamereng@gmail.com>
2018-01-18 12:55:56 +0200

    Handle buttons clicks in home screen

--- app/src/main/java/elsaghier/example/com/appssquare/MainActivity.java
+++ app/src/main/java/elsaghier/example/com/appssquare/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import elsaghier.example.com.appssquare.First_Task.FirstTask;
+import elsaghier.example.com.appssquare.Fourth_Task.MapTask;
 import elsaghier.example.com.appssquare.Second_Task.SecondTask;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,12 +19,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnClick(View view) {
-        Intent intent;
+        Intent intent = null;
         int id = view.getId();
         if (id == R.id.firstTaskBtn)
             intent = new Intent(this, FirstTask.class);
         else if (id == R.id.secondTaskBtn)
             intent = new Intent(this, SecondTask.class);
+        else if (id == R.id.third_task)
+            Toast.makeText(this, "لسه معملتهوش شوف غيره :D ", Toast.LENGTH_SHORT).show();
+        else if (id == R.id.fourth_task)
+            intent = new Intent(this, MapTask.class);
         else
             intent = null;
 
